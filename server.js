1f3179b2eb88bb8896e1e8d2c9592ec483f3d86a
casperwang <casperwang2014@gmail.com>
2022-11-18 10:52:45 +0800

    v1.0.0

--- server.js
+++ server.js
@@ -5,58 +5,61 @@ const express = require('express');
 const app = express();
 
 class Meta {
-  constructor(row, col, player_cnt) {
+  constructor(row, col, real_player_cnt, ai_player_cnt) {
     this.row = row;
     this.col = col;
-    this.player_cnt = player_cnt;
+    this.player_cnt = real_player_cnt + ai_player_cnt;
 
-    this.G = new Game(this.row, this.col, 0.2, 0.02, player_cnt);
+    this.G = new Game(this.row, this.col, 0.2, 0.02, this.player_cnt);
 
     this.players = new Array(this.player_cnt+1);
+    const get_player_type = (player_id) => {
+      if (player_id === 0) return 'admin';
+      if (player_id <= real_player_cnt) return 'real';
+      return 'ai';
+    }
     for (let player_id = 0; player_id <= this.player_cnt; player_id++) {
       this.players[player_id] = {
         team: player_id,
-        type: 'ai'
+        type: get_player_type(player_id)
       };
     }
-    this.players[0].type = 'admin';
-    this.players[1].type = 'human';
-
-    this.servers = new Array(this.player_cnt+1);
-    this.ios = new Array(this.player_cnt+1);
   }
 }
 
-let M = new Meta(25, 50, 8);
+let server = require('http').Server(app).listen(5000, () => {
+  console.log('open server');
+});
+let io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"]
+  }
+});
 
-for (let i = 0; i <= M.player_cnt; i++) {
-  if (M.players[i].type === 'ai') continue;
-  M.servers[i] = require('http').Server(app).listen(5000 + i, () => {
-    console.log('open server ' + (5000+i) + '!');
-  });
-  M.ios[i] = require('socket.io')(M.servers[i], {
-    cors: {
-      origin: '*',
-      methods: ["GET", "POST"]
-    }
-  });
-  M.ios[i].on('connection', socket => {
-    console.log('success connect with player ' + i;
-    socket.on('addMove', move => {
-      M.G.players[move.player_id-1].addMove(move.p, move.d, move.is_half);
-    });
-    socket.emit('initialization', M.G);
-  });
+var M;
+
+let addMove = (move) => {
+  if (M == null) return;
+  M.G.players[move.player_id-1].addMove(move.p, move.d, move.is_half);
 }
 
-setInterval(async () => {
-  await M.G.board_next_step();
-  for (let player_id = 0; player_id <= M.player_cnt; player_id++) {
-    if (M.players[player_id].flag === false) continue;
-    if (M.players[player_id].type === 'ai') {
-      s1(M.G.players[player_id-1]);
-    } else {
-      M.ios[player_id].emit('gameState', M.G);
-    }
-  }
-}, 10);
+io.on('connection', socket => {
+  socket.on('addMove', move => {
+    addMove(move);
+  });
+  socket.on('ready', setting => {
+    M = new Meta(setting.row, setting.col, setting.real_player_cnt, setting.ai_player_cnt);
+    io.emit('gameStart');
+    setInterval(async () => {
+      if (M == null) return;
+      await M.G.board_next_step();
+      for (let player_id = 1; player_id <= M.player_cnt; player_id++) {
+        if (M.players[player_id].flag === false) continue;
+        if (M.players[player_id].type === 'ai')
+          s1(M.G.players[player_id-1]);
+      }
+      io.emit('gameState', M.G);
+    }, 100);
+  });
+});
